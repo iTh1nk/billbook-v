6c75f978d5d9138ce2e7d783701e176e50090628
iTh1nk <fnchao@hotmail.com>
2020-02-07 23:29:29 -0800

    changes

--- routes/routes.js
+++ routes/routes.js
@@ -29,7 +29,7 @@ module.exports = (app, passport) => {
   app.post("/api/updatecycle/", isLoggedInAPI, controller.updateCycle);
 
   app.post("/api/newstatement/", isLoggedInAPI, controller.newStatement);
-  app.post("/api/updatestatement/", controller.updateStatement);
+  app.post("/api/updatestatement/", isLoggedInAPI, controller.updateStatement);
   app.get("/api/getstatement/", isLoggedInAPI, controller.getStatement);
 
   app.get("/api/getuser/", isLoggedInAPI, controller.getUser);

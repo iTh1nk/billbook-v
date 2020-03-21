6e770bfd7372b82d35ab842f64c41baa3bcad5a0
iTh1nk <fnchao@hotmail.com>
2020-03-21 15:19:12 -0700

    add iframe for COVID19 data

--- client/src/components/We0mmm.js
+++ client/src/components/We0mmm.js
@@ -12,8 +12,10 @@ class We0mmm extends Component {
     //   width: "18em",
     //   boxShadow: "20px 20px 50px black"
     // };
+    const ifWeb = "https://coronavirus.1point3acres.com/#stat";
+    // https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik
     return (
-      <div style={{width: "18em", marginLeft: "auto", marginRight: "auto"}}>
+      <div style={{ width: "18em", marginLeft: "auto", marginRight: "auto" }}>
         <a href="/login/">
           <img
             // style={imgStyle}
@@ -22,6 +24,14 @@ class We0mmm extends Component {
             // className="App-logo"
             alt="No Logins? No Worries!"
           />
+          <hr />
+          <iframe
+            id="inlineFrameExample"
+            title="Inline Frame Example"
+            width="300"
+            height="350"
+            src={ifWeb}
+          ></iframe>
         </a>
       </div>
     );

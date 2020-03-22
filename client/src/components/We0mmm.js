863bf4399ef2960cff67bc6fe36edf546234cffc
iTh1nk <fnchao@hotmail.com>
2020-03-21 22:01:23 -0700

    add iframe for COVID19 data

--- client/src/components/We0mmm.js
+++ client/src/components/We0mmm.js
@@ -14,26 +14,35 @@ class We0mmm extends Component {
     // };
     const ifWeb = "https://coronavirus.1point3acres.com/#stat";
     // const ifWeb = "https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik";
+    const ifWebStyle = {
+      width: "15.5em",
+      marginLeft: "auto",
+      marginRight: "auto"
+    };
     return (
-      <div style={{ width: "18em", marginLeft: "auto", marginRight: "auto" }}>
-        <a href="/login/">
-          <img
-            // style={imgStyle}
-            className="img-style"
-            src="/favicon.ico"
-            // className="App-logo"
-            alt="No Logins? No Worries!"
-          />
+      <>
+        <div style={{ width: "15em", marginLeft: "auto", marginRight: "auto" }}>
+          <a href="/login/">
+            <img
+              // style={imgStyle}
+              className="img-style"
+              src="/favicon.ico"
+              // className="App-logo"
+              alt="No Logins? No Worries!"
+            />
+          </a>
           <hr />
+        </div>
+        <div style={ifWebStyle}>
           <iframe
             id="inlineFrameExample"
             title="Inline Frame Example"
-            width="300"
+            width="250"
             height="320"
             src={ifWeb}
           ></iframe>
-        </a>
-      </div>
+        </div>
+      </>
     );
   }
 }

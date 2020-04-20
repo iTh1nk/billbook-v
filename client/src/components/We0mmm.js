3c4c78e74a8f8752fce6eeee2ec7c758ebcc14b3
iTh1nk <fnchao@hotmail.com>
2020-04-19 23:03:55 -0700

    embed APL music

--- client/src/components/We0mmm.js
+++ client/src/components/We0mmm.js
@@ -15,10 +15,10 @@ class We0mmm extends Component {
     const ifWeb = "https://coronavirus.1point3acres.com/#stat";
     // const ifWeb = "https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik";
     const ifWebStyle = {
-      width: "14em",
+      width: "18em",
       marginLeft: "auto",
       marginRight: "auto",
-      boxShadow: "20px 20px 50px black"
+      boxShadow: "20px 20px 50px black",
     };
     // const wStyle = {
     //   marginTop: ".8em",
@@ -45,12 +45,25 @@ class We0mmm extends Component {
         {/* <div style={wStyle}></div> */}
         <hr />
         <div style={ifWebStyle}>
-          <iframe
+          {/* <iframe
             id="inlineFrameExample"
             title="Inline Frame Example"
             width="230"
             height="350"
             src={ifWeb}
+          ></iframe> */}
+          <iframe
+            allow="autoplay *; encrypted-media *;"
+            frameborder="0"
+            height="150"
+            style={{
+              width: "100%",
+              maxWidth: "660px",
+              overflow: "hidden",
+              background: "transparent",
+            }}
+            sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+            src="https://embed.music.apple.com/us/album/if-i-were-a-song/1274389969?i=1274389973"
           ></iframe>
         </div>
       </>

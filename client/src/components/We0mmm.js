9ee0510a4b1cc5670027d3d5b33cbb252186533f
iTh1nk <fnchao@hotmail.com>
2020-04-30 16:42:57 -0700

    update toaster notification

--- client/src/components/We0mmm.js
+++ client/src/components/We0mmm.js
@@ -7,7 +7,7 @@ function We0mmm() {
   useEffect(() => {
     toaster.notify(
       <div style={{ fontWeight: "bold", color: "#e95421" }}>
-        "04-09 Bill Statement is READY!”
+        "Cycle 04-09 Bill Statement is READY!”
       </div>,
       {
         duration: null,
